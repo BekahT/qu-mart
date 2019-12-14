9428083f8e30a26c4b1ac25117096bf861f86489
BekahT <Rebekah.treadwell@gmail.com>
2019-12-13 22:32:07 -0500

    added add to cart button and alert

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 
 import { products } from "../products";
+import { CartService } from '../cart.service';
 
 @Component({
   selector: "app-product-details",
@@ -10,8 +11,16 @@ import { products } from "../products";
 })
 export class ProductDetailsComponent implements OnInit {
   product;
+  
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert(product.name + " has been added to the cart.");    
+  }
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+    ) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
