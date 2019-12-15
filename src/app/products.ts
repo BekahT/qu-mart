e7466727ed316662ed3524529aebaf7c793cb747
BekahT <Rebekah.treadwell@gmail.com>
2019-12-15 17:38:07 -0500

    add more products, lower alert threshold

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,41 +1,77 @@
 export const products = [
   {
     name: "Rice Cooker, 10 Cups",
-    price: 713,
+    price: 712.99,
     image: "assets/img/riceCooker10.jpg", 
     description:
       "A 10 cup induction heating rice cooker with a LCD touch screen, in rose gold"
   },
   {
     name: "Rice Cooker, 6 Cups",
-    price: 450,
+    price: 449.99,
     image: "assets/img/riceCooker6.jpg", 
     description:
       "A 6 cup stainless steel rice cooker with ventiliation pipe system"
   },
   {
     name: "Cabbage Kimchi",
-    price: 10,
+    price: 9.49,
     image: "assets/img/cabbageKimchi.jpg", 
     description: ""
   },
   {
     name: "Cubed Radish Kimchi",
-    price: 10,
+    price: 9.49,
     image: "assets/img/radishKimchi.jpg", 
     description: ""
   },
   {
     name: "Cucumber Kimchi",
-    price: 10,
+    price: 9.49,
     image: "assets/img/cucumberKimchi.jpg", 
     description: ""
   },
   {
     name: "Beef Kalbi Marinade",
-    price: 7,
+    price: 6.99,
     image: "assets/img/kalbiMarinade.jpg", 
     description: "Korean Style BBQ Sauce Beef Kalbi Marinade 1.85lb"
+  },
+  {
+    name: "Shin Ramyun",
+    price: 4.99,
+    image: "assets/img/shinRamyun.jpg", 
+    description: "Pack of 4. Soft and chewy noodle combined with spicy beef broth creates the perfect recipe for any time of day or setting."
+  },
+  {
+    name: "Shrimp Cracker",
+    price: 1.29,
+    image: "assets/img/shrimpCracker.jpg", 
+    description: "Shrimp Cracker is a crunchy, aromatic snack with delicious Shrimp flavor."
+  },
+  {
+    name: "Rice, 15lb",
+    price: 21.99,
+    image: "assets/img/rice15.jpg", 
+    description: ""
+  },
+  {
+    name: "Gochujang",
+    price: 7.99,
+    image: "assets/img/gochujang.jpg", 
+    description: "Hot pepper paste, 1.1lb"
+  },
+  {
+    name: "Rubber Gloves",
+    price: 2.99,
+    image: "assets/img/gloves.jpg", 
+    description: "Best rubber gloves for cleaning and washing dishes. Pink."
+  },
+  {
+    name: "Banana Milk",
+    price: 7.99,
+    image: "assets/img/bananaDrink.jpg", 
+    description: "Banana milk is a delicious and energizing beverage with creamy banana flavor."
   }
 ];
 
