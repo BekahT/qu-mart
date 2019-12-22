798371d3023f4063f88fbe7a3d2c846108bc854e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-22 22:37:17 +0000

    Deploy BekahT/qu-mart to github.com/BekahT/qu-mart.git:gh-pages

---
+++ assets/shipping.ts
@@ -0,0 +1,14 @@
+export const Shipping: Array<object> = [
+    {
+        "type": "Overnight",
+        "price": 25.55
+    },
+    {
+        "type": "Two-Day",
+        "price": 10.75
+    },
+    {
+        "type": "Regular",
+        "price": 5.99
+    }
+];
